5d59fc17b6e94fa1f6a6088b850db96c200a3fde
nishatlea <nishataurora1956@gmail.com>
2022-11-08 15:59:12 +0600

    scaffold CRUD operation done , although delete not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :friends
   root 'home#index'
   get 'home/index'
   get 'home/about'
+  # delete "/friends/:id"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

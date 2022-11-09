ea2eff97cc6b4837293e4680fa35442045bd1cc0
nishatlea <nishataurora1956@gmail.com>
2022-11-09 16:59:55 +0600

    Sign up, Sign in, Sign out,Edit profile items in navbar using devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #configuration needed for devise user management
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 end
